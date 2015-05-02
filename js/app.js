da831eb59f4c80ebc7d91d7c3e9548d59b1b2800
Colin Fahy <cfahy@compete.com>
2015-05-02 16:50:32 -0400

    Added effects and microphone

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ var source;
 var audio_buffer;
 var analyser;
 var is_playing = false;
+var is_playing_buffer = false;
+var is_playing_osc = false;
+var is_playing_mic = false;
+var pre_gain;
+var post_gain;
+var source_node = {};
+var dest_node = {};
 window.start_time = 0;
 window.pause_time = 0;
 
@@ -10,7 +17,6 @@ var analyser2;
 var scriptProcessor2;
 
 var ctx_spectrum;
-var gradient;
 
 var ctx_spectrogram;
 var tempCtx;
@@ -26,6 +32,15 @@ var ctx_cepstrum;
 
 var num_plots = 5;
 var num_oscillators = 0;
+var oscillators = {};
+var osc_gains = {};
+
+//Effects
+var effects = {};
+
+var max_delay = 5.0;
+
+var microphone;
 
 window.addEventListener('load', init, false);
 function init() {
@@ -41,20 +56,72 @@ function init() {
         //source = context.createBufferSource();
         //source.connect(context.destination);
 
+        pre_gain = context.createGain();
+        post_gain = context.createGain();
+        pre_gain.connect(post_gain);
+        post_gain.connect(context.destination);
+
+        source_node['node'] = pre_gain
+        dest_node['node'] = post_gain;
+        initEffects(pre_gain, post_gain);
+
+        source_node['dest'] = dest_node;
+        dest_node['source'] = source_node;
+
         analyser = context.createAnalyser();
         analyser.smoothingTimeConstant = 0.3;
-        analyser.fftSize = 1024;
-        analyser.connect(context.destination);
+        analyser.fftSize = 2048;
+        post_gain.connect(analyser);
 
         analyser2 = context.createAnalyser();
         analyser2.smoothingTimeConstant = 0;
-        analyser2.fftSize = 1024;
+        analyser2.fftSize = 2048;
+        post_gain.connect(analyser2);
 
         initCanvas();
         document.getElementById('local_file').addEventListener('change', handleFileSelect, false);
         document.getElementById('fft_size').addEventListener('change', fftSizeChange, false);
         document.getElementById('loop').addEventListener('change', loopClick, false);
+
     }
+    navigator.getUserMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia ||
+                             navigator.msGetUserMedia;
+}
+
+function initEffects() {
+    var filter = {};
+    filter['node'] = context.createBiquadFilter();
+    effects['filter'] = filter;
+
+    var compressor = {};
+    compressor['node'] = context.createDynamicsCompressor();
+    effects['compressor'] = compressor;
+
+    var delay = {};
+    delay['node'] = context.createDelay(max_delay);
+    effects['delay'] = delay;
+
+    var convolver = {};
+    convolver['node'] = context.createConvolver();
+    effects['convolver'] = convolver;
+    load_convolution_buffer('audio/Hall.wav');
+}
+
+function load_convolution_buffer(filename) {
+    ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.open('GET', filename, true);
+    ajaxRequest.responseType = 'arraybuffer';
+
+    ajaxRequest.onload = function() {
+      var audioData = ajaxRequest.response;
+      context.decodeAudioData(audioData, function(buffer) {
+          effects['convolver']['node'].buffer = buffer;
+        }, function(e){"Error with decoding audio data" + e.err});
+    }
+
+    ajaxRequest.send();
 }
 
 function initCanvas() {
@@ -62,9 +129,8 @@ function initCanvas() {
     ctx_spectrum = canvas.getContext('2d');
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-    gradient = make_gradient();
 
-    scriptProcessor = context.createScriptProcessor(1024);
+    scriptProcessor = context.createScriptProcessor(512);
     scriptProcessor.connect(context.destination);
     analyser.connect(scriptProcessor);
     scriptProcessor.onaudioprocess = drawSpectrum;
@@ -79,7 +145,7 @@ function initCanvas() {
     tempCanvas.width=canvas2.width;
     tempCanvas.height=canvas2.height;
 
-    scriptProcessor2 = context.createScriptProcessor(1024);
+    scriptProcessor2 = context.createScriptProcessor(512);
     scriptProcessor2.connect(context.destination);
     analyser2.connect(scriptProcessor2);
     scriptProcessor2.onaudioprocess = drawSpectrogram;
@@ -88,7 +154,7 @@ function initCanvas() {
     ctx_oscilloscope = canvas3.getContext('2d');
     canvas3.width = canvas3.offsetWidth;
     canvas3.height  = canvas3.offsetHeight;
-    scriptProcessor3 = context.createScriptProcessor(1024);
+    scriptProcessor3 = context.createScriptProcessor(512);
     scriptProcessor3.connect(context.destination);
     analyser2.connect(scriptProcessor3);
     scriptProcessor3.onaudioprocess = drawOscilloscope;
@@ -97,7 +163,7 @@ function initCanvas() {
     ctx_waveform = canvas4.getContext('2d');
     canvas4.width  = canvas4.offsetWidth;
     canvas4.height = canvas4.offsetHeight;
-    scriptProcessor4 = context.createScriptProcessor(1024);
+    scriptProcessor4 = context.createScriptProcessor(512);
     scriptProcessor4.connect(context.destination);
     analyser2.connect(scriptProcessor4);
     scriptProcessor4.onaudioprocess = drawWaveform;
@@ -148,10 +214,11 @@ function drawWaveform() {
     analyser2.getByteTimeDomainData(array);
     var canvas = ctx_waveform.canvas;
 
-    //audioDuration = document.getElementById('player').duration;
-    audioDuration = audio_buffer.duration;
-    //totalSamples = audioDuration * context.sampleRate;
-    totalSamples = audio_buffer.length;
+    if (audio_buffer) {
+        totalSamples = audio_buffer.length;
+    }else{
+        totalSamples = 999999;
+    }
     timeBinWidth = (canvas.width)/(totalSamples/scriptProcessor4.bufferSize);
     if(isNaN(timeBinWidth)){
         return;
@@ -182,7 +249,7 @@ function drawSpectrum() {
     var array =  new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
     ctx_spectrum.clearRect(0, 0, ctx_spectrum.canvas.width, ctx_spectrum.canvas.height);
-    ctx_spectrum.fillStyle=gradient;
+    ctx_spectrum.fillStyle=make_gradient(ctx_spectrum);
     for ( var i = 0; i < (array.length); i++ ){
         var value = ctx_spectrum.canvas.height*array[i]/256;
         var binWidth = ctx_spectrum.canvas.width/analyser.frequencyBinCount;
@@ -193,7 +260,7 @@ function drawSpectrum() {
 function drawSpectrogram() {
     if (!is_playing || !document.getElementById("spectrogram")) {
     //if (document.getElementById('player').paused){
-            return;
+        return;
     }
     //Copy current canvas to temp canvas
     canvas = ctx_spectrogram.canvas;
@@ -202,10 +269,13 @@ function drawSpectrogram() {
     var array = new Uint8Array(analyser2.frequencyBinCount);
     analyser2.getByteFrequencyData(array);
 
-    //audioDuration = document.getElementById('player').duration;
-    audioDuration = audio_buffer.duration;
-    //totalSamples = audioDuration * context.sampleRate;
-    totalSamples = audio_buffer.length;
+    if (is_playing_buffer) {
+        audioDuration = audio_buffer.duration;
+        totalSamples = audio_buffer.length;
+    }else{
+        audioDuration = 1;
+        totalSamples = 999999;
+    }
     timeBinWidth = canvas.width/(totalSamples/scriptProcessor2.bufferSize);
     timeBinWidth = Math.max(timeBinWidth, 1);
     freqBinWidth = canvas.height/analyser2.frequencyBinCount;
@@ -229,6 +299,7 @@ function drawCepstrum() {
     }
     var array =  new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
+    var binWidth = ctx_cepstrum.canvas.width/(analyser.frequencyBinCount/2);
 
     var data = new complex_array.ComplexArray(array.length);
     data.map(function(value, i, n) {
@@ -238,11 +309,10 @@ function drawCepstrum() {
     mag_array = data.magnitude();
 
     ctx_cepstrum.clearRect(0, 0, ctx_cepstrum.canvas.width, ctx_cepstrum.canvas.height);
-    ctx_cepstrum.fillStyle=gradient;
+    ctx_cepstrum.fillStyle=make_gradient(ctx_cepstrum);
     for ( var i = 1; i < (mag_array.length/2); i++ ){
         var value = 20 * Math.log(mag_array[i]) / Math.LN10;
 
-        var binWidth = ctx_cepstrum.canvas.width/(analyser.frequencyBinCount/2);
         ctx_cepstrum.fillRect((i-1)*binWidth,ctx_cepstrum.canvas.height,binWidth*3/4,-value);
     }
 };
@@ -259,6 +329,37 @@ $(document).ready(function(){
         var clicked = $(this).find('a:first').attr('href');
         $(clicked).fadeIn('fast');
         e.preventDefault();
+
+        // Enable Mic
+        if ($(this).find('a:first').attr('href') == "#mic") {
+            if (!microphone) {
+                var errorCallback = function(e) {
+                  alert("Microphone disabled");
+                };
+                try {
+                    navigator.getUserMedia({audio: true}, function(stream) {
+                      microphone = context.createMediaStreamSource(stream);
+                      microphone.connect(pre_gain);
+                    }, errorCallback);
+                }
+                catch(e) {
+                    alert('Microphone is not supported in this browser');
+                }
+            }else{
+                microphone.connect(pre_gain);
+            }
+            is_playing_mic = true;
+            is_playing = true;
+        // Disable Mic
+        } else {
+            if (microphone) {
+                microphone.disconnect();
+                is_playing_mic = false;
+                if (!is_playing_osc && !is_playing_buffer) {
+                    is_playing = false;
+                }
+            }
+        }
     }).eq(0).addClass('current');
 
 
@@ -271,7 +372,7 @@ $(document).ready(function(){
         // var audio = document.getElementById('player');
         // audio.currentTime=0;
         // audio.play();
-        if (is_playing){
+        if (is_playing_buffer){
             source.stop();
         } else {
             start_audio(audio_buffer, 0);
@@ -281,7 +382,7 @@ $(document).ready(function(){
 
 
     $('#tts_form').submit(function () {
-        if (is_playing){
+        if (is_playing_buffer){
             source.stop();
         } else {
             var lang = $.trim($("#language").val());
@@ -307,41 +408,75 @@ $(document).ready(function(){
     });
 
     $('#add_oscillator').click(function () {
+        $('#rem_oscillator').show();
         num_oscillators += 1;
-        var osc_elem = "<div name='osc"+num_oscillators+"'>"
-                        +"<select>"
+        var id = 'osc' + num_oscillators;
+        var osc_elem = "<div id='"+id+"'>"
+                        +"<select id='"+id+"_type' class='osc_type'>"
                             +"<option value='sine'>Sine</option>"
-                            +"<option value=square'>Square</option>"
+                            +"<option value='square'>Square</option>"
                             +"<option value='triangle'>Triange</option>"
                             +"<option value='sawtooth'>Sawtooth</option>"
-                        +"</select>"
-                        +"<label for='"+num_oscillators+"_freq'>Frequency:</input>"
-                        +"<input type='number' id='"+num_oscillators+"_freq' value='440' min='0' max='50000' class='freq' />"
-                        +"<label for='"+num_oscillators+"_amp'>Amplitude:</input>"
-                        +"<input type='number' id='"+num_oscillators+"_amp' value='1' min='0' max='1' step='0.1' class='amp' />"
+                        +"</select><br/>"
+                        +"<label for='"+id+"_freq'>Frequency:</label>"
+                        +"<input type='number' id='"+id+"_freq' value='440' min='0' max='50000' class='osc_freq' />"
+                        +"<label for='"+id+"_amp'>Amplitude:</label>"
+                        +"<input type='number' id='"+id+"_amp' value='0.5' min='0' max='1' step='0.1' class='osc_amp' />"
                        +"</div>"
         $('#oscillators').append(osc_elem);
+        osc_node = context.createOscillator();
+        osc_gain = context.createGain();
+        osc_gain.gain.value = 0.5;
+        osc_node.connect(osc_gain);
+        osc_gain.connect(pre_gain);
+        osc_node.start(0);
+        is_playing = true;
+        is_playing_osc = true;
+        oscillators[id] = osc_node;
+        osc_gains[id] = osc_gain;
     });
 
     $('#rem_oscillator').click(function () {
         $('#oscillators').children().last().remove();
+        var id = 'osc' + num_oscillators;
+        oscillators[id].stop();
+        delete oscillators[id];
+        delete osc_gains[id];
+
         num_oscillators -= 1;
+        if (num_oscillators <= 0) {
+            $('#rem_oscillator').hide();
+            is_playing_osc = false;
+            if (!is_playing_buffer && !is_playing_mic) {
+                is_playing = false;
+            }
+        }
+    });
+
+    $('#oscillators').on('change', '.osc_type', function() {
+        var id = $(this).parent().attr('id');
+        oscillators[id].type = this.value;
+    });
+
+    $('#oscillators').on('change', '.osc_freq', function() {
+        var id = $(this).parent().attr('id');
+        oscillators[id].frequency.value = this.value;
+    });
+
+    $('#oscillators').on('change', '.osc_amp', function() {
+        var id = $(this).parent().attr('id');
+        osc_gains[id].gain.value = this.value;
     });
 
     $(document).on('keypress', function(e) {
         var tag = e.target.tagName.toLowerCase();
         if ( e.which === 32 && tag != 'input' && tag != 'textarea'){
-            if(is_playing){
+            e.preventDefault();
+            if(is_playing_buffer){
                 source.stop(0);
             }else{
                 start_audio(audio_buffer, 0);
             }
-            // var audio = document.getElementById('player');
-            // if (audio.paused) {
-            //       audio.play();
-            // } else {
-            //       audio.pause();
-            // }
         }
     });
 
@@ -356,12 +491,56 @@ $(document).ready(function(){
        ctx_spectrogram.canvas.height = ctx_spectrogram.canvas.offsetHeight;
        tempCtx.canvas.height = ctx_spectrogram.canvas.offsetHeight;
        ctx_cepstrum.canvas.height = ctx_cepstrum.canvas.offsetHeight;
-       gradient = make_gradient();
+    });
+
+    $('.effect').on('change', function(e) {
+        effect = effects[e.target.id];
+        if (e.target.checked) {
+            source_node['node'].disconnect();
+            source_node['node'].connect(effect['node']);
+            effect['node'].connect(dest_node['node']);
+            source_node['dest'] = effect;
+            effect['source'] = source_node;
+            effect['dest'] = dest_node;
+            source_node = effect;
+        }else{
+            effect['node'].disconnect();
+            effect['source']['node'].disconnect();
+            effect['source']['node'].connect(effect['dest']['node']);
+            effect['source']['dest'] = effect['dest'];
+            effect['dest']['source'] = effect['source'];
+            source_node = effect['source'];
+            dest_node = effect['dest'];
+        }
+    });
+
+
+    // Effect settings
+    $('#filter_freq').on('change', function(e) {
+        effects['filter']['node'].frequency.value = e.target.value;
+    });
+    $('#filter_Q').on('change', function(e) {
+        effects['filter']['node'].Q.value = e.target.value;
+    });
+    $('#filter_type').on('change', function(e) {
+        effects['filter']['node'].type = e.target.value;
+    });
+    $('#compressor_thresh').on('change', function(e) {
+        effects['compressor']['node'].threshold.value = e.target.value;
+    });
+    $('#compressor_knee').on('change', function(e) {
+        effects['compressor']['node'].knee.value = e.target.value;
+    });
+    $('#delay_time').on('change', function(e) {
+        effects['delay']['node'].delayTime.value = e.target.value;
+    });
+    $('#impulse_response').on('change', function(e) {
+        load_convolution_buffer(e.target.value);
     });
 });
 
-function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+function handleFileSelect(e) {
+    var files = e.target.files; // FileList object
     var file = files[0];
     var reader = new FileReader();
     // Closure to capture the file information.
@@ -377,16 +556,16 @@ function handleFileSelect(evt) {
     reader.readAsArrayBuffer(file);
 }
 
-function loopClick(evt){
+function loopClick(e) {
     //var audio = document.getElementById('player');
     //audio.loop = checkboxElement.checked;
     if (source){
-        source.loop = evt.target.checked;
+        source.loop = e.target.checked;
     }
 }
 
-function fftSizeChange(evt){
-    var newValue = evt.target.value;
+function fftSizeChange(e) {
+    var newValue = e.target.value;
     analyser.fftSize = newValue;
     analyser2.fftSize = newValue;
 }
@@ -403,23 +582,26 @@ function start_audio(audioBuffer, start_time){
         ctx_waveform.clearRect(0,0,ctx_waveform.canvas.width,ctx_waveform.canvas.height);
     }
     source = context.createBufferSource();
-    source.connect(analyser);
-    source.connect(analyser2);
+    source.connect(pre_gain);
     source.buffer = audio_buffer;
     source.start(0,start_time);
     is_playing = true;
+    is_playing_buffer = true;
     $('.button').val('\u00A0\u258C\u258C');
     if ($('#loop').is(":checked")){
             source.loop = true;
     }
     source.onended = function() {
-        is_playing = false;
+        is_playing_buffer = false;
+        if (!is_playing_osc && !is_playing_mic) {
+            is_playing = false;
+        }
         $('.button').val('\u25BA');
     }
 }
 
-function make_gradient(){
-    var grad = ctx_spectrum.createLinearGradient(0,0,0,ctx_spectrum.canvas.height);
+function make_gradient(ctx){
+    var grad = ctx.createLinearGradient(0,0,0,ctx.canvas.height);
     grad.addColorStop(1,'#000000');
     grad.addColorStop(0.85,'#ff0000');
     grad.addColorStop(0.35,'#ffff00');
